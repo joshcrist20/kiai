ddd39b40d7c5561684641e44e052b52d0a928636
Josh Crist <joshcrist20@gmail.com>
2015-09-11 14:44:03 -0600

    Landing Page without Kiai Images

--- kiai/settings.py
+++ kiai/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'kiai.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(MAIN_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
