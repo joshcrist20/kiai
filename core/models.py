7649de0efc5ead875e9174b8cd2e7d120c63a472
Josh Crist <joshcrist20@gmail.com>
2015-09-11 22:27:39 -0600

    Images and Added Fields

--- core/models.py
+++ core/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 from django.core.urlresolvers import reverse
+
+import os
+import uuid
+
+def upload_to_location(instance, filename):
+    blocks = filename.split('.')
+    ext = blocks[-1]
+    filename = "%s.%s" % (uuid.uuid4(), ext)
+    instance.title = blocks[0]
+    return os.path.join('uploads/', filename)
+
 # Create your models here.
 
 class Location(models.Model):
@@ -7,6 +18,7 @@ class Location(models.Model):
 	description = models.TextField(null=True, blank=True)
 	address = models.TextField(null=True, blank=True)
 	hours = models.TextField(null=True, blank=True)
+	image_file = models.ImageField(upload_to=upload_to_location, null=True, blank=True)
 	created_at = models.DateTimeField(auto_now_add=True)
 
 	def __unicode__(self):
