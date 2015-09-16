fe0cfc6dfb29c60d600ee9d18debd0e23fc176dc
Josh Crist <joshcrist20@gmail.com>
2015-09-16 10:15:40 -0600

    Removed secret keys

--- kiai/settings.py
+++ kiai/settings.py
@@ -128,7 +128,7 @@ os.path.join(MAIN_DIR, 'static'),
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 AWS_S3_FORCE_HTTP_URL = True
 AWS_QUERYSTRING_AUTH = False
-AWS_SECRET_ACCESS_KEY = os.environ.get('4EnU2kxvd/bzOMhcCIr5Jc28NDmlpUZpa9ene9NS')
-AWS_ACCESS_KEY_ID = os.environ.get('AKIAJRRWPNHXUZ5GHUQQ')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWSSecretKey')
+AWS_ACCESS_KEY_ID = os.environ.get('AWSAccessKeyId')
 
 AWS_STORAGE_BUCKET_NAME = 'kiaiapp'
